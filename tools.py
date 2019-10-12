67f7840b9126a03e02b91b951cbbc17f467e9185
Lludion <ulysse.remond@outlook.fr>
2019-10-12 17:32:38 +0200

    Adding the "tools.py" file, containing basic practical functions

---
+++ tools.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+def xyinbounds(mx,my,btn):
+    b1xmin,b1xmax,b1ymin,b1ymax = btn.boundaries()
+    return b1xmin <= mx and mx <= b1xmax and b1ymin <= my and my <= b1ymax
+
+def no_reaction():
+    return True, False
+
+def create_img(dct):
+    for img in dct:
+        dct[img] = pygame.image.load(dct[img]).convert_alpha()
+    return dct
