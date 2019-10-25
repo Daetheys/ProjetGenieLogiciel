a7ce779471994fc3e5cd441a32a8245540cee49f
EngDez <enguerrand.dezerces@gmail.com>
2019-10-25 16:28:14 +0200

    set attributes of classes of campaign to private

--- campaign/level_sequence.py
+++ campaign/level_sequence.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 from map_point import Map_Point
+from level import Level
 
 
 class Level_Sequence(Map_Point):
     
     def __init__(self):
+        self.__levels = []
         
-        self.levels = []
+    def set_levels(self, levels):
+        self.__levels = levels
+        
+    def get_levels(self):
+        return self.__levels
