7b566360eba31d16702f025fc342a3f4269efa40
lludion <ulysse.remond@outlook.fr>
2019-10-29 18:01:08 +0100

    Automata & Sprite Scheduler v1
    - Automata class is properly defined
    - Automata can be parsed&constructed from json file
    - SpriteScheduler can load an automaton from the dict
    - SpriteScheduler has an internal automaton, on which operation can be performed

--- engine/automata.py
+++ engine/automata.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
+
 from os import getcwd
 from sys import path
 path0 = getcwd()
-path0 += "/error"
-path.append(path)
+path0 += "/error"#works with "/Desktop/ProjetGenieLogiciel/error" for me
+path.append(path0)
 from exception import TransitionUndefined
+from collections import defaultdict
 
 class Automata:
 	def __init__(self,name,states,tt,cs):
 		self.name = name
 		self.states = states
-		self.tt = tt#transition table
+		self.tt = tt#transition table (dictionnary)
 		self.cs = cs#current state
 
-def create_automaton(data,name=None,states=None,tt=None,cs=None,jsonparse=True):
+def create_automaton(data,name=None,states=None,tt=None,cs=None,jsonparse=True,nocomment=True):
 	"""creates an automaton:
 		- from the json parser if jsonparse
 		- from the explicitly given data else
@@ -22,31 +24,52 @@ def create_automaton(data,name=None,states=None,tt=None,cs=None,jsonparse=True):
 		name(str) is the name
 		n	(int) is the number of states
 		tt	(str) is of the following format:
-			s,a → b; t,c → q
-			
+			state,char→state; state,char→state ...
+			(where 'state' is an int, char is a char)
+
+	note : tt may be empty. n still has to be followed by |.
 	note : the initial state is always set to 0 in a json parser
+	note : if nocomment is disabled, str can be of the format: name|n|tt|comments
 	"""
-	def newfun(f,old_state,char,new_state):
-		def f2(m,d):
-			if m,d == old_state,char:
-				return new_state
-			else:
-				return f(m,d)
+	def throwTU(arg=None):
+		"""throws the exception TransitionUndefined
+		is used in the defaultdict intialization"""
+		raise TransitionUndefined(arg)
+
+	def parseint(data,i,marker='|'):
+		""" parses an int in a str.
+		n : the integer
+		i : the position of the first non-integer bit (is marker or len(data))"""
+		n = 0
+		while i < len(data) and data[i] != marker:
+			n = n * 10 + int(data[i])
+			i += 1
+		return n,i
+
 	if jsonparse:
 		name = ""
-		i = 0#curseur
-		while i < len(data) and data[i] != '|':
+		i = 0#cursor
+		while i < len(data) and data[i] != '|':#reading the name
 			name += data[i]
 			i += 1
+		assert data[i] == "|"
 		i += 1
-		nstate = 0
-		while i < len(data) and data[i] != '|':
-			nstate = nstate * 10 + data[i]
+		nstate,i = parseint(data,i)#number of states
+		assert data[i] == "|"
+		states = list(range(nstate))
+		tt = defaultdict(throwTU)
+		i += 1
+		while i < len(data) and data[i] != '|':#adding the transitions
 			i += 1
-		states = list(range(data[nstate]))
-		def tt(n,ch):
-			return Error
-		while i < len(data) and data[i] != '|':
-			i += 1
+			s,i = parseint(data,i,',')
+			assert data[i] == ','
+			c = data[i+1]
+			assert data[i+2] == '→'
+			i += 3
+			s2,i = parseint(data,i,';')
+			tt[s,c] = s2
+		if nocomment: assert i == len(data)
+		return Automata(name,states,tt,0)
+
 	else:
 		return Automata(name,states,tt,cs)
--- engine/spriteScheduler.py
+++ engine/spriteScheduler.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
-from automata import create_automaton
+from automata import *
 
 def load_automata():
 	"""loads all automata"""
 	global dict_ata
-	with open("data/json/automata.json", "r") as read_file:
-		dict_ata=json.load(read_file,object_hook=create_automaton)
+	with open("../data/json/automata.json", "r") as read_file:
+		dict_ata = json.load(read_file,object_hook=create_automaton)
 
 class SpriteSchedule:
-    def load_automaton():
+	def __init__(self,name=""):
+		self.name = name
+		self.ata = None
+	def load_automaton(self):
 		for automat in dict_ata:
 			if automat.name == self.name:
 				self.ata = automat
 
-    def step(char):
-		self.cs = self.tt(self.cs,char)
+	def step(self,char):
+		try:
+			self.ata.cs = self.ata.tt[self.ata.cs,char]
+		except TransitionUndefined:
+			print("No transition from state"+str(self.ata.cs)+"with letter"+char)
