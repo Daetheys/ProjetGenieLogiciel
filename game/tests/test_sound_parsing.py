b10898bceb6c9eb68914bc87ca8482f5cb350be5
Edlyr <elies.harington@gmail.com>
2019-11-08 17:00:28 +0100

    Corrected the sound parsing unit test - Replaced wrongs paths with correct ones

--- game/tests/test_sound_parsing.py
+++ game/tests/test_sound_parsing.py
@@ -3,14 +3,10 @@ import os
 import numpy as np
 
 path = os.getcwd()
-music_path = path + "/data/musique/test_musique2/120rythmeternaire.mp3"
-path += "/game/level_generation"
 sys.path.append(path + "/game/level_generation")
-sys.path.append(path + "/data/musique/test_musique")
-sys.path.append(path + "/data/musique/test_musique2")
 
 from sound_parser import bpm_info
 
-
-(_,tempo,_) = bpm_info("120rythmeternaire.mp3")
-assert(np.round(tempo) == 240)
+def test_tern():
+	(_,tempo,_) = bpm_info(path + "/data/tests_musique2/120rythmeternaire.mp3")
+	assert(np.round(tempo) == 240)
