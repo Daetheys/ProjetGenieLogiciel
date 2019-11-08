38ab5aac8d5948ea66f3624553aaf37a5b65c8bc
lludion <ulysse.remond@outlook.fr>
2019-11-08 14:38:42 +0100

    Updating class inheritances
    -Npc inherits now from character
    -Some inconsistent space pb solved in spriteNode
    -Campaign moved 
    - Method has no argument "error" in transformable solved

--- game/campaign/items.py
+++ game/campaign/items.py
@@ -1,23 +1,24 @@
 
-class ItemType(self):
+class Consommable(Item):
 	""" Type of an item. Usually strings of len 3."""
+	#ItemType, type of the item: consommable, key item, passive item, etc..
 	def __init__(self,kind,args=None):
+		Item.__init__(self)
 		self.kind = kind#3-char str: con for consommable, key, pas, etc..
-		self.args = args#other optional arguments
 
-class Item(self):
+class Item:
 	""" Items of the game:
 	has name, picture, type
 	"""
 	def __init__(self,name='Apple',pic=None,kind=None):
 		self.name = name#str, name
 		self.pic = pic#pygame.Surface, picture
-		self.kind = kind#ItemType, type of the item: consommable, key item, passive item, etc..
+		
 
 	def __repr__(self):
 		return "Item :" + self.name + " of type :" +  self.kind
 
-class Inventory(self):
+class Inventory:
 	""" Inventory of a character. """
 	def __init__(self,items=[]):
 		self.items = items#Item list
