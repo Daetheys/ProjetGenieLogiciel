983240ebe86dc97e5f06a27b8189c4d9164248b8
lludion <ulysse.remond@outlook.fr>
2019-11-08 14:38:42 +0100

    Updating class inheritances
    -Npc inherits now from character
    -Some inconsistent space pb solved in spriteNode
    -Campaign moved 
    - Method has no argument "error" in transformable solved

--- game/campaign/npc.py
+++ game/campaign/npc.py
@@ -1,8 +1,12 @@
+import Character
 
-class Npc():
+class Npc(Character):
 	"""Class representing a non-playable character : 
-	it has a dialogue"""
-	def __init__(self,dial="",to_sell=[]):
+	it has a dialogue
+	Npc (that can talk, sell items, etc..)
+	"""
+	def __init__(self,super,dial="",to_sell=[]):
+		Character.__init__(self)
 		self.dial = dial#str, Dialogue
 		self.to_sell = to_sell#Item list, list of saleable items
 
