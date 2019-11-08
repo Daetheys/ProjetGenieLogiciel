0bbc3c73e2760ccd93be4321b32090578cc757b0
Edlyr <elies.harington@gmail.com>
2019-11-08 15:49:00 +0100

    Improved tempo and first beat detection

--- level_generation/sound_parser.py
+++ level_generation/sound_parser.py
@@ -1,4 +1,14 @@
+DEBUG = False
+
 import librosa.core, librosa.beat
+import numpy as np
+
+if(DEBUG):
+	import matplotlib
+	matplotlib.use('TkAgg')
+	import matplotlib.pyplot as plt
+
+threshold = 0.7
 
 def bpm_info(file):
 	"""
@@ -8,6 +18,27 @@ def bpm_info(file):
 			- le nombre de beats
 	"""
 	y, sr = librosa.core.load(file, sr=None)
-	tempo, beat_frames = librosa.beat.beat_track(y=y,sr=sr)
+	onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+	pulse = librosa.beat.plp(onset_envelope=onset_env, sr=sr, tempo_min=30, tempo_max=300)
+
+	beat_frames = []
+	beat_intensities = []
+
+	for i in range(1,len(pulse)-1):
+		if(pulse[i-1] < pulse[i] and pulse[i] > pulse[i+1] and pulse[i] > threshold):
+			beat_frames.append(i)
+			beat_intensities.append(pulse[i])
+
 	beat_times = librosa.core.frames_to_time(frames=beat_frames,sr=sr)
-	return beat_times[0], tempo, len(beat_times)
+
+	tempos = 60/(beat_times[1:]-beat_times[:-1])
+	tempo = np.sum(tempos[1:-1])/(len(tempos)-2)
+
+	if(DEBUG):
+		print(tempo)
+		plt.bar(beat_times,beat_intensities, 0.05)
+		plt.plot(beat_times[1:-2],tempos[1:-1])
+		plt.show()
+
+
+	return beat_times[1], tempo, (len(beat_times)-1)
