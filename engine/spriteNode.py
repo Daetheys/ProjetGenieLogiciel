dfa6373078545a1e2e4e93067b1a2dcfe424228b
lludion <ulysse.remond@outlook.fr>
2019-11-08 15:01:14 +0100

    creation of map.py, indentation levels corrected
    for PA

--- engine/spriteNode.py
+++ engine/spriteNode.py
@@ -4,12 +4,12 @@ class SpriteNode(Node):
 	def __init__(self):
 		self.__state = None #stay,move,damaged,collision,
 		self.__giver = None #
-	            
-		def set_scheduler(self,sche):
-	            self.__giver = sche
 
-	    def set_state(self,state):
-	            self.__state = state
+	def set_scheduler(self,sche):
+		self.__giver = sche
 
-	    def get_state(self):
-	            return self.__state
+	def set_state(self,state):
+		self.__state = state
+
+	def get_state(self):
+		return self.__state
