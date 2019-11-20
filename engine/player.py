c93b411146edada4eb4bd6116a9156be11d1f979
Daetheys <daetheys@gmail.com>
2019-11-20 15:06:24 +0100

    Add player class

--- engine/player.py
+++ engine/player.py
@@ -1,5 +1,25 @@
 from controlableNode import ControlableNode
+from solidePlatform import SolidPlatform
 
 class Player(ControlableNode):
     def __init__(self):
-        super().__init__()
+        ControlableNode.__init__(self)
+        self.set_hit_box(Hitbox(Rect(-1,-2,2,4)))
+        self.set_rigid_body(True)
+        self.set_sps(None)
+        self.get_sps().load_automaton()
+        self.get_sps().load_sprites()
+        jump_strength = 5
+        can_jump = False
+
+    def jump(self):
+        if self.can_jump:
+            self.set_speed(self.get_speed()+Vector(0,-self.jump_strength))
+            self.can_jump = True
+
+    def allow_jump(self):
+        self.can_jump = True
+
+    def collide(self,o):
+        if isinstance(o,SolidePlatform):
+            self.allow_jump()
