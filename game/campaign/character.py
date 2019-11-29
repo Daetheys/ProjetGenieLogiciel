8d82a1bda204198e296bb5f48f171f325ca89297
lludion <ulysse.remond@outlook.fr>
2019-11-29 17:49:35 +0100

    Pretty printing of Character class
    -now also displays inventory, color, size of picture

--- game/campaign/character.py
+++ game/campaign/character.py
@@ -2,7 +2,7 @@ from items import *
 
 
 class Character():
-	"""Class representing a character : 
+	"""Class representing a character :
 	it has a name,
 	a picture,
 	talks with a certain color,
@@ -14,18 +14,18 @@ class Character():
 		self.inv = inv#Inventory, inventory of the character (usually is None)
 
 	def __repr__(self):
-		return "Character :" + self.name
-		
+		return "Character : " + self.name + "\tSurface:" + str(self.pic) +  "\tColor:" + str(self.ctalk) +  "\tInventory:" + str(self.inv)
+
 	def set_inventory(self,items):
 		for item in items:
 			if item.type == "csm":
 				self.inv[item] += items[item]
 			else:
 				self.inv[item] = items[item]
-				
+
 	def get_inventory(self):
 		return self.inv
-		
+
 	def is_in_inventory(self,item):
 		if self.inv[item] > 0:
 			return True
