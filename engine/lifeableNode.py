05ffe0cd63c782d00693188aa23760f7fd15d937
Daetheys <daetheys@gmail.com>
2019-12-07 13:39:24 +0100

    Minor changes in life bar so that it's even more beautiful

--- engine/lifeableNode.py
+++ engine/lifeableNode.py
@@ -5,7 +5,7 @@ class LifeableNode(ControlableNode):
     def __init__(self):
         super().__init__()
         self.max_pv = 5
-        self.pv = 5
+        self.pv = 3
         self.small = True #small == True -> little health bar over head of the lifeable node
 
     def set_max_pv(self,v):
@@ -24,4 +24,5 @@ class LifeableNode(ControlableNode):
         print("upper aff",self)
         (px,py,w,h) = self.get_pos_camera(distorsion,self.get_hit_box())
         super().aff(fen,distorsion,dt)
+        pygame.draw.rect(fen,(0,0,0),pygame.Rect(px+w/8,py-12,w*0.8,4))
         pygame.draw.rect(fen,(0,255,0),pygame.Rect(px+w/8,py-12,w*self.get_percent_life()*0.8,4))
