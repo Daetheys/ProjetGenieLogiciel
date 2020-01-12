3ba10c7457264cdb59b361302d75a9dc3466b225
Daetheys <daetheys@gmail.com>
2020-01-12 20:30:36 +0100

    Added a small description of performances with cProfile #40 Added log files of performances and updated the tex file

--- init.py
+++ init.py
@@ -4,10 +4,7 @@ Can·A·Baelde v1.1
 """
 import sys
 from game.game import Game
-import cProfile
-import re
 
 if __name__ == '__main__':
-    cProfile.run("re.compile(Game())")
-    #Game()
+    Game()
     print(sys.path)
