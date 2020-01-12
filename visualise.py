07743298f0c6a58315f16c918790cc5d06057612
lludion <ulysse.remond@outlook.fr>
2020-01-13 00:48:16 +0100

    Visualiser is fully operational #36
    -the window fits the screen
    -can be activated via the command line
    -comments added

--- visualise.py
+++ visualise.py
@@ -2,31 +2,42 @@ import pygame
 import time
 from game.level_generation.level_generator import generate_level
 from engine.vector import Vector
+from argparse import ArgumentParser
 
 pygame.init()
-W = 1600
-H = 900
+i = pygame.display.Info()
+W = int( i.current_w * 9/10 )
+H = int( i.current_h * 9/10 )
 FEN = pygame.display.set_mode((W,H))
 
-def visualise(gl):
-    pygame.display.flip()
-    FEN.set_alpha(None)
-    (mini_x,maxi_x,mini_y,maxi_y) = gl.get_size_level()
-    mini_x -= abs(mini_x)*1.1
-    maxi_x += int(abs(maxi_x)**0.7)
-    mini_y -= abs(mini_y)*1.1
-    maxi_y += abs(maxi_y)*1.1
-    gl.camera.set_position(Vector(mini_x,mini_y))
-    dx = maxi_x-mini_x
-    dy = maxi_y-mini_y
-    gl.camera.set_dimension(Vector(dx,dy))
-    #gl.camera.set_position(Vector(-2000,-250))
-    #gl.camera.set_dimension(Vector(45000,500))
-    gl.load_camera(FEN)
-    gl.aff(0.1,gl.objects)
-    pygame.draw.rect(FEN,(255,255,255),pygame.Rect(W*5/6,H*5/6,15,50/dy*H))
-    pygame.display.flip()
-    time.sleep(20)
+def visualise(filepath="data/your music/"):
+	"""a Visualiser for music generated level, as required for part 2.
+	
+	The white rectangle is the maximal jump height."""
 
-gl = generate_level("data/musics/cool.mp3")
-visualise(gl)
+	parser = ArgumentParser()
+	parser.add_argument("filename",
+						help="name of the file you want to see.",
+						type=str, default="cool.wav", nargs='?')
+	name = parser.parse_args().filename
+	print("File chosen : "+name)
+	filepath += name
+	gl = generate_level(filepath)
+	pygame.display.flip()
+	FEN.set_alpha(None)
+	(mini_x,maxi_x,mini_y,maxi_y) = gl.get_size_level()
+	mini_x -= int(abs(mini_x)*1.1)
+	maxi_x += int(abs(maxi_x)**0.7)
+	mini_y -= int(abs(mini_y)*1.1)
+	maxi_y += int(abs(maxi_y)*1.1)
+	gl.camera.set_position(Vector(mini_x,mini_y))
+	dx = maxi_x-mini_x
+	dy = maxi_y-mini_y
+	gl.camera.set_dimension(Vector(dx,dy))
+	gl.load_camera(FEN)
+	gl.aff(0.1,gl.objects)
+	pygame.draw.rect(FEN,(255,255,255),pygame.Rect(W*5/6,H*5/6,15,50/dy*H))
+	pygame.display.flip()
+	time.sleep(20)
+
+visualise()
