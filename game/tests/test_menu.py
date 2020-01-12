0a637fb8f32b6b4db1be802d65edc822398417b2
lludion <ulysse.remond@outlook.fr>
2020-01-12 23:28:00 +0100

    Added explanations to the test_menu.py. Closed #6
    -The forks are required, and mess up with xvfb and pytest in a terrible way. We can't test graphically the levels. This is now explained in test_menu.py

--- game/tests/test_menu.py
+++ game/tests/test_menu.py
@@ -6,22 +6,42 @@ import signal
 from hypothesis import *
 from hypothesis.strategies import *
 from time import sleep
+from game.game import Game
 
+def test_explanation():
+    """
+    All tests are now deactivated, again. xvfb still fiddles with forks and kills
+    Thanks to the Game class, one can launch several displays in one run.
+    However, to interact with the python process, one need another process, because
+    the Game() enters an infinite event-catching loop. Therefore, we must fork()
+    However, this fork interacts on a deep level with pytest+xvfb in a bad way.
+    As a consequence, we cannot test automatically the GUI of this project.
+    """
+    return 0
 
 """
-All tests are now deactivated, again. xvfb still fiddles with forks and kills
-Thanks to the launcher class, one can launch several displays in one run.
+def test_launching():
+    #testing if the game launches correctly, then killing the process
+    newpid = fork()
+    if newpid == 0:#This first fork is for launching the menu and the killer in parallel
+        #we launch the game
+        Game()#contains an infinite loop
+    else:
+        #we kill it
+        sleep(4)
+        os.kill(newpid,signal.SIGKILL)
+        _exit(0)
+        sleep(2)
+        os.kill(os.getpid(), signal.SIGKILL)
 """
 
+"""
 def test_escape():
     #testing if the escape button works at the root of the menu,
     #introducing various easy ways to test efficiently
-    pass
-    """
     newpid = fork()
     if newpid:#This first fork is for launching the menu and pyautogui in parallel
-        #pass
-        Launcher()
+        Game()
         _exit(0)
         sleep(2)
         os.kill(os.getpid(), signal.SIGKILL)
@@ -30,7 +50,7 @@ def test_escape():
         #could be added
         sleep(1)#wait for the menu to initialize
         pyautogui.press('escape')
-    """
+"""
 
 """
 def test_exit_button():
@@ -38,7 +58,7 @@ def test_exit_button():
 
     newpid = fork()
     if newpid:
-        Launcher()
+        Game()
         _exit(0)
         sleep(5)
         os.kill(os.getpid(), signal.SIGKILL)
@@ -57,7 +77,7 @@ def test_inputs(txt):
     #test if whether game does crash with randomized inputs
     newpid = fork()
     if newpid:#This first fork is for launching the menu and the typer in parallel
-        Launcher()
+        Game()
         _exit(0)
         sleep(5)
         os.kill(os.getpid(), signal.SIGKILL)
