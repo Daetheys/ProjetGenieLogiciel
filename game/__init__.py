de47016b00a0340299c9b09feb98ec9387d2a3f2
lludion <ulysse.remond@outlook.fr>
2020-01-13 03:02:36 +0100

    Documentation Wiki, Fix #42 #44 #32
    -it contains accurate descriptions of all the Python classes that we created, and also other html pages

---
+++ game/__init__.py
@@ -0,0 +1,7 @@
+"""
+
+The game folder contains all the aspects of the game that are not directly related to the actual gameplay.
+
+For instance, it contains all the classes using the scenario, the menu, the initialization, the level generation,...
+
+"""
--- init.py
+++ init.py
@@ -1,6 +1,10 @@
 """
 Can·A·Baelde v2.0
 
+this is a game where the player must jump to survive, while constantly running. 
+You can play on levels of the campaigns, but also on ones that can be generated from your
+own musics
+
 """
 
 from game.game import Game
